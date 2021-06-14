8e4916674509ebb717337f74d820ed10182e7187
Adem ATİCİ <adematicis41@gmail.com>
2021-06-14 11:39:30 +0300

    First Commit

---
+++ app/src/main/java/com/zafertugcu/araczamanlamasistemi/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zafertugcu.araczamanlamasistemi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zafertugcu.araczamanlamasistemi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+}
